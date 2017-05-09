a06f20c2eb5b416bc27fde21f96395cbbd4523f6
Joey Harrington <jtharrington@mail.fhsu.edu>
2017-05-09 14:22:37 -0500

    Final Project

--- Project-1-jtharrington/Python/sqrt.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-
-import sys
-
-for line in sys.stdin:
- line = line.strip()
- num = float(line)
- num = num**0.5
-print num
--- Project-1-jtharrington/Python/square.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-
-import sys
-
-for line in sys.stdin:
- line = line.strip()
- num = float(line)
- num = num**2
-print num
